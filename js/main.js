b27ce09822b7c0bbdd52be61c5bd5591025eb09e
Inna-B10 <inna.b10.kodehode@gmail.com>
2024-02-21 11:18:34 +0100

    display all games (cards) - done

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
-const url = "https://free-to-play-games-database.p.rapidapi.com/api/games";
+const urlAll = "https://free-to-play-games-database.p.rapidapi.com/api/games";
 const output = document.getElementById("output");
 
-async function getGameData() {
+async function getGamesData(url) {
   const options = {
     method: "GET",
     headers: {
@@ -14,14 +14,66 @@ async function getGameData() {
     const response = await fetch(url, options);
     if (response.ok) {
       const result = await response.json();
-      console.log(result);
+      return result;
     } else {
       console.log("Could not fetch data");
       output.innerText = "Something got wrong";
     }
   } catch (error) {
     console.error(error);
-    output.innerText = "An error occurred when fetching list of games";
+    output.innerText = "An error occurred when fetching data";
   }
 }
-getGameData();
+const data = await getGamesData(urlAll);
+showGames(data);
+
+function createNode(node, attributes) {
+  const el = document.createElement(node);
+  for (let key in attributes) {
+    el.setAttribute(key, attributes[key]);
+  }
+  return el;
+}
+
+function showGames(array) {
+  let alt = "";
+  let iconPath = "";
+  array.forEach((element) => {
+    const card = createNode("div", {
+      class: "flex column card",
+    });
+    const thumb = createNode("img", {
+      src: `${element.thumbnail}`,
+      alt: `${element.title}`,
+      title: `${element.title}`,
+    });
+    const title = createNode("h2", {});
+    title.innerText = `${element.title}`;
+    const shortDesc = createNode("p", {});
+    shortDesc.innerText = `${element.short_description}`;
+    const details = createNode("div", {
+      class: "flex",
+    });
+    if (element.genre === "Web Browser") {
+      alt = "Browser based game";
+      iconPath = "../images/web.png";
+    } else {
+      alt = "Game for Windows";
+      iconPath = "../images/windows.png";
+    }
+    const platform = createNode("img", {
+      src: `${iconPath}`,
+      alt: `${alt}`,
+      title: `${alt}`,
+    });
+    const genre = createNode("p", {});
+    genre.innerText = `${element.genre}`;
+    details.appendChild(platform);
+    details.appendChild(genre);
+    card.appendChild(thumb);
+    card.appendChild(title);
+    card.appendChild(shortDesc);
+    card.appendChild(details);
+    output.appendChild(card);
+  });
+}
